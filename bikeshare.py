57862ce4bc45a39b0f0b5be32ea141a449a1c6d3
UmaRaghavan <umapsft82@yahoo.com>
2019-02-15 13:42:32 -0500

    Removed the assignment statements for popular_hour popular_day and its occurrences. Added the value_counts directly to the print statement in functions category_one() and category_two()

--- bikeshare.py
+++ bikeshare.py
@@ -16,14 +16,14 @@ CITY_NAME = {1: 'Chicago',
              2: 'New York City',
              3: 'Washington'}
 MONTH_DATA = {0: 'all months', 1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June'}
-             
+
 def welcome_message():
-     
+
     """
     Asks user to select a city to analyze.
     Then passes control to function category_input()
     """
-    
+
     print('\n*****************Hello! Welcome to Bikeshare Data*****************')
     print('\nSelect the option for the city you wish to get statistics about:')
     print('\n1. Chicago')
@@ -31,7 +31,7 @@ def welcome_message():
     print('3. Washington')
     print('4. Exit')
 
-    
+
     while True:
         try:
             city_num = int(input('Choose an option from 1 to 4: '))
@@ -40,7 +40,7 @@ def welcome_message():
         except:
             print('\nIncorrect option!')
 
-        
+
     if (city_num == 4):
         print('\nThanks for accessing Bikeshare Data. Goodbye')
         exit()
@@ -49,16 +49,16 @@ def welcome_message():
         rec_count = df['Start Time'].count()
         print('Number of records for {} is {}.'.format(CITY_NAME[city_num],rec_count))
         category_input(city_num)
-        
-        
-        
+
+
+
 def category_input(city_num):
     """
     Asks user to specify a month and option to analyze for the selected city.
     Args:
-        (int) city_num - number corresponding to that city 
+        (int) city_num - number corresponding to that city
    """
-   
+
 
     while True:
         try:
@@ -67,12 +67,12 @@ def category_input(city_num):
                 break
         except:
             print('Incorrect option!')
-                
+
     print('\nSelect a category for data analysis about {} for {}:'.format(CITY_NAME[city_num],MONTH_DATA[month_num]))
     print('\n 1. Popular time to travel')
     print(' 2. Popular start/end station')
     print(' 3. Trip Duration')
-    print(' 4. User Info')  
+    print(' 4. User Info')
     print(' 5. All of the above')
     print(' 6. Raw Data')
     print(' 7. Exit (Go to the main screen)')
@@ -83,10 +83,10 @@ def category_input(city_num):
                 break
         except:
             print('Incorrect option!')
-            
-        
-    print('\nRetrieving data about {} for {}...' .format(CITY_NAME[city_num],MONTH_DATA[month_num]))        
-    
+
+
+    print('\nRetrieving data about {} for {}...' .format(CITY_NAME[city_num],MONTH_DATA[month_num]))
+
     if option_num == 1:
         category_one(city_num,month_num,option_num)
     if option_num == 2:
@@ -105,17 +105,17 @@ def category_input(city_num):
         category_six(city_num,month_num)
     if option_num == 7:
         welcome_message()
-            
+
 def post_category(city_num):
-    
+
     """
     Prompts the user whether to continue with the analysis or to go to the main menu.
 
     Args:
         (int) city - name of the city to analyze
-  
+
     """
-    
+
     print('\n********************************************************')
     print('\nEnter 1 for more city analysis')
     print('Enter 2 for Main Menu')
@@ -126,15 +126,15 @@ def post_category(city_num):
                  break
         except:
             print('Choose an option 1 or 2: ')
-        
-    
+
+
     if post_num == 1:
         category_input(city_num)
     else:
         welcome_message()
-            
-        
-    
+
+
+
 def category_one(city_num,month_num,option_num):
     """
     Loads data for the specified city and filters by month and category if applicable.
@@ -144,7 +144,7 @@ def category_one(city_num,month_num,option_num):
         (int) city - name of the city to analyze
         (int) month - name of the month to filter by, or "all" to apply no month filter
         (int) option - used to determine if user picked one or all category
-    
+
     """
     start_time = datetime.now()
     #load data from csv of the corresponding city into the dataframe
@@ -159,23 +159,19 @@ def category_one(city_num,month_num,option_num):
     df['hour'] = df['Start Time'].dt.hour
     # the most common hour (from 0 to 23)
     print('\n******************Popular Times to Travel******************')
-    popular_hour = df['hour'].mode().loc[0]
-    print('\nMost Popular Hour\t\t: ', popular_hour)
-    common_hour = df['hour'].value_counts().iloc[0]
-    print('Number of occurences\t: ', common_hour)
+    print('\nMost Popular Hour\t\t: ', df['hour'].mode().loc[0])
+    print('Number of occurences\t: ', df['hour'].value_counts().iloc[0])
     #find the popular weekday
     df['day_of_week'] = df['Start Time'].dt.weekday_name
-    popular_day = df['day_of_week'].mode().loc[0]
-    print('Most popular day\t\t: ' , popular_day)
-    common_day = df['day_of_week'].value_counts().iloc[0]
-    print('Number of occurences\t: ', common_day)
+    print('Most popular day\t\t: ' , df['day_of_week'].mode().loc[0])
+    print('Number of occurences\t: ', df['day_of_week'].value_counts().iloc[0])
     end_time = datetime.now()
     print('\nProcessing Time: {}'.format(end_time - start_time))
-   
+
 
     if (option_num != 5):
         post_category(city_num)
-    
+
 
 def category_two(city_num,month_num,option_num):
     """
@@ -186,7 +182,7 @@ def category_two(city_num,month_num,option_num):
         (int) city - name of the city to analyze
         (int) month - name of the month to filter by, or "all" to apply no month filter
         (int) option - used to determine if user picked one or all category
-    
+
     """
     start_time = datetime.now()
     df = pd.read_csv(CITY_DATA[city_num])
@@ -196,28 +192,25 @@ def category_two(city_num,month_num,option_num):
     df['month'] = df['Start Time'].dt.month
     if (month_num != 0):
         df=df[df['month'] == month_num]
-    
+
     print('\n*****************Popular Start/End Stations*****************')
     #common start station
     popular_startstation =df['Start Station'].mode().loc[0]
     print('\nMost popular start station\t\t: ' ,popular_startstation)
-    common_start = df['Start Station'].value_counts().iloc[0]
-    print('Number of occurences for Start Station\t: ', common_start)
+    print('Number of occurences for Start Station\t: ', df['Start Station'].value_counts().iloc[0])
     #common end station
-    popular_endstation = df['End Station'].mode().loc[0]
-    print('Most popular end station\t\t: ' ,popular_endstation)
-    common_end = df['End Station'].value_counts().iloc[0]
-    print('Number of occurences for End Station\t: ', common_end)
+    popular_endstation =
+    print('Most popular end station\t\t: ' ,df['End Station'].mode().loc[0])
+    print('Number of occurences for End Station\t: ', df['End Station'].value_counts().iloc[0])
     #common start/end combo trip
     df['combo_trip'] = df['Start Station'] +','+ df['End Station']
-    popular_trip_start_end = df['combo_trip'].mode().loc[0]
-    print('Most Common trip\t\t\t: ', popular_trip_start_end)  
+    print('Most Common trip\t\t\t: ', df['combo_trip'].mode().loc[0])
     end_time = datetime.now()
     print('\nProcessing Time: {}'.format(end_time - start_time))
-    
+
     if (option_num != 5):
         post_category(city_num)
-    
+
 def category_three(city_num,month_num,option_num):
     """
     Loads data for the specified city and filters by month and category if applicable.
@@ -227,7 +220,7 @@ def category_three(city_num,month_num,option_num):
         (int) city - name of the city to analyze
         (int) month - name of the month to filter by, or "all" to apply no month filter
         (int) option - used to determine if user picked one or all category
-    
+
     """
     start_time = datetime.now()
     df = pd.read_csv(CITY_DATA[city_num])
@@ -237,7 +230,7 @@ def category_three(city_num,month_num,option_num):
     df['month'] = df['Start Time'].dt.month
     if (month_num != 0):
         df=df[df['month'] == month_num]
-    
+
     print('\n******************Trip Duration Information******************')
     Total_Trip_Time= df['Trip Duration'].sum()
     print('\nTotal Trip time\t\t: ', Total_Trip_Time)
@@ -254,7 +247,7 @@ def category_three(city_num,month_num,option_num):
     print('\nProcessing Time: {}'.format(end_time - start_time))
     if (option_num != 5):
         post_category(city_num)
-    
+
 def category_four(city_num,month_num,option_num):
     """
     Loads data for the specified city and filters by month and category if applicable.
@@ -264,7 +257,7 @@ def category_four(city_num,month_num,option_num):
         (int) city - name of the city to analyze
         (int) month - name of the month to filter by, or "all" to apply no month filter
         (int) option - used to determine if user picked one or all category
-        
+
     """
     print('\n********************User Information********************')
     start_time = datetime.now()
@@ -275,7 +268,7 @@ def category_four(city_num,month_num,option_num):
     df['month'] = df['Start Time'].dt.month
     if (month_num != 0):
         df=df[df['month'] == month_num]
-    
+
     #count of each user_types
     user_types = df['User Type'].value_counts()
     print ('\nUser Types:')
@@ -287,10 +280,10 @@ def category_four(city_num,month_num,option_num):
         print(gender_count)
         #common birth year
         Bir_year = df['Birth Year'].mode().loc[0]
-        print('\nCommon Birth Year\t\t: ', Bir_year) 
+        print('\nCommon Birth Year\t\t: ', Bir_year)
         #Total birth counts
-        Birth_count = df['Birth Year'].value_counts().iloc[0]  
-        print('Number of Births\t\t: ', Birth_count ) 
+        Birth_count = df['Birth Year'].value_counts().iloc[0]
+        print('Number of Births\t\t: ', Birth_count )
         #Earliest birth year
         Earliest_year = df['Birth Year'].min()
         print('Earliest Birth Year\t\t: ', Earliest_year)
@@ -299,10 +292,10 @@ def category_four(city_num,month_num,option_num):
         print('Recent Birth Year\t\t: ', Recent_year)
         end_time = datetime.now()
         print('\nProcessing Time: {}'.format(end_time - start_time))
-        
+
     if (option_num != 5):
         post_category(city_num)
-        
+
 def category_six(city_num,month_num):
     """
     Loads data for the specified city and filters by month.
@@ -311,9 +304,9 @@ def category_six(city_num,month_num):
     Args:
         (int) city - name of the city to analyze
         (int) month - name of the month to filter by, or "all" to apply no month filter
-        
-     """   
-        
+
+     """
+
     df = pd.read_csv(CITY_DATA[city_num])
     # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
@@ -334,15 +327,14 @@ def category_six(city_num,month_num):
                         break
                 except:
                     print('Enter y or n: ')
-                
+
             if(raw_option.lower() == 'n'):
                 break
             else:
                 continue
-            
-    post_category(city_num)            
-    
-    
-        
-welcome_message()        
-        
+
+    post_category(city_num)
+
+
+
+welcome_message()
